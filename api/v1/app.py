5a17031eb19929d83cd8cc52c999bd4a0e2d8f83
Levyslater <lawre@Xshader>
2024-08-14 11:56:46 +0300

    fix documentation_on app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
-"""Creates a new flask app and registers the blue print"""
-from flask import Flask
+"""
+This module creates a Flask application and registers the API blueprint.
+It also sets up CORS, handles teardown, and customizes error responses.
+"""
+
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from flask import jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 
-# Enable CORS
+# Enable CORS for all routes under /api/v1/*
 CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
 
+# Register the API blueprint
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """closes the storage on teardown"""
+    """Closes the storage on teardown of the Flask app context."""
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """returns a JSON error message for 404 errors"""
+    """Returns a JSON-formatted 404 error response."""
     return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
+    # Get the host and port from environment variables or default to '0.0.0.0' and 5000
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     PORT = getenv('HBNB_API_PORT', 5000)
+    
+    # Run the Flask app with threading and debug mode enabled
     app.run(host=HOST, port=PORT, threaded=True, debug=True)
